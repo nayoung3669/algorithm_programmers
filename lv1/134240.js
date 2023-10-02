dea5fad568a2c43bef49cb11bb212b026c3e1b44
nayoung3669 <skdud3669@gmail.com>
2023-10-02 15:47:55 +0900

    final 134240

--- lv1/134240.js
+++ lv1/134240.js
@@ -3,9 +3,6 @@ function solution(food) {
     for (let i=1; i<food.length; i++) {
         arr.push(String(i).repeat(Math.floor(food[i] / 2)))
     }
-    arr.push(0)
-    for (let j=arr.length-2; j>=0; j--) {
-        arr.push(arr[j])
-    }
-    return arr.join("")
+    return arr.join("") + "0" + arr.reverse().join("")
+    
 }
