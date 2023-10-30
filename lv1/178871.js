0dd9db5ce7d2c73f80811d49830b541cd19e82b7
nayoung3669 <skdud3669@gmail.com>
2023-10-30 22:03:06 +0900

    updated 178871

--- lv1/178871.js
+++ lv1/178871.js
@@ -1,13 +1,17 @@
 function solution(players, callings) {
   let idx = 0;
+  let temp = "";
+  let m = new Map();
+  players.map((n, index) => {
+    m.set(n, index);
+  });
   for (let i = 0; i < callings.length; i++) {
-    idx = players.indexOf(callings[i]);
-    players = [
-      ...players.slice(0, idx - 1),
-      players[idx],
-      players[idx - 1],
-      ...players.slice(idx + 1, players.length),
-    ];
+    idx = m.get(callings[i]);
+    m.set(players[idx], idx - 1);
+    m.set(players[idx - 1], idx);
+    temp = players[idx - 1];
+    players[idx - 1] = callings[i];
+    players[idx] = temp;
   }
   return players;
 }
