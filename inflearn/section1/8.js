c255cd8dc806e54e9da603a9b5c8ea581ad2a2cc
nayoung3669 <skdud3669@gmail.com>
2024-03-11 13:17:42 +0900

    refactoring section1

--- inflearn/section1/8.js
+++ inflearn/section1/8.js
@@ -1,5 +1,13 @@
 function solution(arr) {
-  arr.sort();
+  let sum = arr.reduce((c, a) => c + a);
+  for (let i = 0; i < 8; i++) {
+    for (let j = i + 1; j < 9; j++) {
+      let a = arr[i] + arr[j];
+      if (sum - a == 100) {
+        return arr.filter((s) => s !== arr[i] && s !== arr[j]);
+      }
+    }
+  }
 }
 
 let arr = [20, 7, 23, 19, 10, 15, 25, 8, 13];
